7891e06a52491be2b0ae2f7dbcacaa5ffb16d002
Farbod909 <rafezyfarbod@gmail.com>
2015-06-17 20:03:39 -0700

    Adds announcements to main page

--- js/navbar.js
+++ js/navbar.js
@@ -9,36 +9,38 @@ $(document).ready(function(){
     $('#prof_drop').hide();
     $('.triangle_thing_prof').hide()
 
+    var fade_speed = 200;
+
     $('#notif_button').click(function(){
-      $('#notif_drop').fadeIn(300)
-      $('.triangle_thing_notif').fadeIn(300)
-      $('#darken').fadeIn(300);
+      $('#notif_drop').fadeIn(fade_speed)
+      $('.triangle_thing_notif').fadeIn(fade_speed)
+      $('#darken').fadeIn(fade_speed);
       $('#notif_button').addClass('active');
       $('#darken').click(function(){
-        $(this).fadeOut(300);
-        $('#notif_drop').fadeOut(300);
-        $('.triangle_thing_notif').fadeOut(300);
+        $(this).fadeOut(fade_speed);
+        $('#notif_drop').fadeOut(fade_speed);
+        $('.triangle_thing_notif').fadeOut(fade_speed);
         $('#notif_button').removeClass('active');
       });
     });
     $('#profile_id').click(function(){
-      $('#prof_drop').fadeIn(300);
-      $('.triangle_thing_prof').fadeIn(300);
-      $('#darken').fadeIn(300);
+      $('#prof_drop').fadeIn(fade_speed);
+      $('.triangle_thing_prof').fadeIn(fade_speed);
+      $('#darken').fadeIn(fade_speed);
       $('#darken').click(function(){
-        $(this).fadeOut(300);
-        $('#prof_drop').fadeOut(300);
-        $('.triangle_thing_prof').fadeOut(300);
+        $(this).fadeOut(fade_speed);
+        $('#prof_drop').fadeOut(fade_speed);
+        $('.triangle_thing_prof').fadeOut(fade_speed);
       });
     });
     $('#profile_name').click(function(){
-      $('#prof_drop').fadeIn(300);
-      $('.triangle_thing_prof').fadeIn(300);
-      $('#darken').fadeIn(300);
+      $('#prof_drop').fadeIn(fade_speed);
+      $('.triangle_thing_prof').fadeIn(fade_speed);
+      $('#darken').fadeIn(fade_speed);
       $('#darken').click(function(){
-        $(this).fadeOut(300);
-        $('#prof_drop').fadeOut(300);
-        $('.triangle_thing_prof').fadeOut(300);
+        $(this).fadeOut(fade_speed);
+        $('#prof_drop').fadeOut(fade_speed);
+        $('.triangle_thing_prof').fadeOut(fade_speed);
       });
     });
     $('#logout_button').click(function(){
