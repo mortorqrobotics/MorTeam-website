55a1ab7e9a63b67072477a79af766ec040cff62c
thatarvinguy <zadeharvin@gmail.com>
2015-06-30 23:07:46 -0700

    Complete overhaul of profile pic functionality-works

--- js/navbar.js
+++ js/navbar.js
@@ -1,85 +1,80 @@
-function getPic(user){
-	$.post("/f/getPic", JSON.stringify({"user": user}), function(response){
-		console.log(typeof(response));
-		console.log(response);
-	});
+function getPic(user) {
+    $("#small_prof_pic").attr("src", "/f/getPic?user=" + user);
 }
-
-$(document).ready(function(){
-	$('#name_link').html(localStorage.firstName);
+getPic(localStorage.username);
+$(document).ready(function() {
+    $('#name_link').html(localStorage.firstName);
 
     // $('#darken').hide();
-		//
+    //
     // $('#notif_drop').hide();
     // $('.triangle_thing_notif').hide()
-		//
+    //
     // $('#prof_drop').hide();
     // $('.triangle_thing_prof').hide()
 
     var fade_speed = 200;
 
-    $('#notif_button').click(function(){
-      $('#notif_drop').fadeIn(fade_speed).removeClass("hidden")
-      $('.triangle_thing_notif').fadeIn(fade_speed).removeClass("hidden")
-      $('#darken').fadeIn(fade_speed).removeClass("hidden")
-      $('#notif_button').addClass('active');
-      $('#darken').click(function(){
-        $(this).fadeOut(fade_speed, function(){
-					$(this).addClass("hidden")
-				});
-        $('#notif_drop').fadeOut(fade_speed, function(){
-					$(this).addClass("hidden")
-				});
-        $('.triangle_thing_notif').fadeOut(fade_speed, function(){
-					$(this).addClass("hidden")
-				});
-        $('#notif_button').removeClass('active');
-      });
+    $('#notif_button').click(function() {
+        $('#notif_drop').fadeIn(fade_speed).removeClass("hidden")
+        $('.triangle_thing_notif').fadeIn(fade_speed).removeClass("hidden")
+        $('#darken').fadeIn(fade_speed).removeClass("hidden")
+        $('#notif_button').addClass('active');
+        $('#darken').click(function() {
+            $(this).fadeOut(fade_speed, function() {
+                $(this).addClass("hidden")
+            });
+            $('#notif_drop').fadeOut(fade_speed, function() {
+                $(this).addClass("hidden")
+            });
+            $('.triangle_thing_notif').fadeOut(fade_speed, function() {
+                $(this).addClass("hidden")
+            });
+            $('#notif_button').removeClass('active');
+        });
     });
-    $('#profile_id').click(function(){
-			getPic(localStorage.user)
-      $('#prof_drop').fadeIn(fade_speed).removeClass("hidden");
-      $('.triangle_thing_prof').fadeIn(fade_speed).removeClass("hidden");
-      $('#darken').fadeIn(fade_speed).removeClass("hidden");
-      $('#darken').click(function(){
-        $(this).fadeOut(fade_speed, function(){
-					$(this).addClass("hidden")
-				});
-        $('#prof_drop').fadeOut(fade_speed, function(){
-					$(this).addClass("hidden")
-				});
-        $('.triangle_thing_prof').fadeOut(fade_speed, function(){
-					$(this).addClass("hidden")
-				});
-      });
+    $('#profile_id').click(function() {
+        $('#prof_drop').fadeIn(fade_speed).removeClass("hidden");
+        $('.triangle_thing_prof').fadeIn(fade_speed).removeClass("hidden");
+        $('#darken').fadeIn(fade_speed).removeClass("hidden");
+        $('#darken').click(function() {
+            $(this).fadeOut(fade_speed, function() {
+                $(this).addClass("hidden")
+            });
+            $('#prof_drop').fadeOut(fade_speed, function() {
+                $(this).addClass("hidden")
+            });
+            $('.triangle_thing_prof').fadeOut(fade_speed, function() {
+                $(this).addClass("hidden")
+            });
+        });
     });
-    $('#profile_name').click(function(){
-			getPic(localStorage.user)
-      $('#prof_drop').fadeIn(fade_speed).removeClass("hidden");
-      $('.triangle_thing_prof').fadeIn(fade_speed).removeClass("hidden");
-      $('#darken').fadeIn(fade_speed).removeClass("hidden");
-      $('#darken').click(function(){
-        $(this).fadeOut(fade_speed, function(){
-					$(this).addClass("hidden")
-				});
-        $('#prof_drop').fadeOut(fade_speed, function(){
-					$(this).addClass("hidden")
-				});
-        $('.triangle_thing_prof').fadeOut(fade_speed, function(){
-					$(this).addClass("hidden")
-				});
-      });
+    $('#profile_name').click(function() {
+        $('#prof_drop').fadeIn(fade_speed).removeClass("hidden");
+        $('.triangle_thing_prof').fadeIn(fade_speed).removeClass("hidden");
+        $('#darken').fadeIn(fade_speed).removeClass("hidden");
+        $('#darken').click(function() {
+            $(this).fadeOut(fade_speed, function() {
+                $(this).addClass("hidden")
+            });
+            $('#prof_drop').fadeOut(fade_speed, function() {
+                $(this).addClass("hidden")
+            });
+            $('.triangle_thing_prof').fadeOut(fade_speed, function() {
+                $(this).addClass("hidden")
+            });
+        });
     });
-    $('#logout_button').click(function(){
-    	localStorage.removeItem('username');
-    	localStorage.removeItem('userToken');
-    	localStorage.removeItem('firstName');
-    	localStorage.removeItem('lastName');
-    	localStorage.removeItem('subdivison');
-    	localStorage.removeItem('phone');
-    	localStorage.removeItem('email');
-    	localStorage.removeItem('teamName');
-    	localStorage.removeItem('teamNumber');
-    	location="login.html";
+    $('#logout_button').click(function() {
+        localStorage.removeItem('username');
+        localStorage.removeItem('userToken');
+        localStorage.removeItem('firstName');
+        localStorage.removeItem('lastName');
+        localStorage.removeItem('subdivison');
+        localStorage.removeItem('phone');
+        localStorage.removeItem('email');
+        localStorage.removeItem('teamName');
+        localStorage.removeItem('teamNumber');
+        location = "login.html";
     });
 });
