6bd5242c1e8f3500e5a25db6baccb070d9f0807f
thatarvinguy <zadeharvin@gmail.com>
2015-08-23 00:22:13 -0700

    Adds viewing user attendance functionality

--- js/main.js
+++ js/main.js
@@ -69,6 +69,17 @@ function updateTeammates(){
 }
 updateTeammates();
 
+function qs(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+}
+
 function getDayOfWeek(m, d, y) {
     m--;
     var hlpr = mtend[m];
